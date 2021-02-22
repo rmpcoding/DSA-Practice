c03da2aeb8ac27c125c316a69c6653d3a8ddabd5
Robert Parsons <rmpcoding@gmail.com>
2021-02-21 21:02:11 -0800

    Adds getLast() method

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -12,21 +12,21 @@ class LinkedList {
         let newHead = new Node(data);
         this.head = newHead;
         if (currentHead) {
-            this.head.setNextNode(currentHead)
+            this.head.setNextNode(currentHead);
         }
     }
 
     addTail(data) {
         let tail = this.head;
-        let newTail = new Node(data)
+        let newTail = new Node(data);
 
         if (!tail) {
-            this.addHead(data)
+            this.addHead(data);
         } else {
-            while(tail.getNextNode()) {
+            while (tail.getNextNode()) {
                 tail = tail.getNextNode();
             }
-            tail.setNextNode(newTail)
+            tail.setNextNode(newTail);
         }
     }
 
@@ -34,25 +34,42 @@ class LinkedList {
         this.head = null;
     }
 
+    getHead() {
+        let head = this.head;
+        head ? console.log(head.data) : console.log('Linked List is empty!');
+    }
+
     getSize() {
         let count = 1;
         let currentNode = this.head;
 
         if (currentNode) {
-            while(currentNode.getNextNode()) {
+            while (currentNode.getNextNode()) {
                 currentNode = currentNode.getNextNode();
-                count++
+                count++;
             }
             console.log(count);
-        } else console.log('Linked List is empty!')
+        } else console.log('Linked List is empty!');
+    }
+
+    getLast() {
+        let tail = this.head;
+        if (!tail) {
+            console.log('Linked List is empty!')
+        } else {
+            while (tail.getNextNode()) {
+                tail = tail.getNextNode();
+            }
+            console.log(tail.data)
+        }
     }
 
     printList() {
-        let currentNode = this.head;6
+        let currentNode = this.head;
         let output = '<head> ';
         while (currentNode) {
-          output += currentNode.data + ' --> ';
-          currentNode = currentNode.getNextNode();
+            output += currentNode.data + ' --> ';
+            currentNode = currentNode.getNextNode();
         }
         output += '<tail>';
         console.log(output);
@@ -61,16 +78,12 @@ class LinkedList {
 
 let linkedList = new LinkedList();
 
-linkedList.getSize()
-linkedList.addHead("I'm the tail!");
+linkedList.addHead("I was once a head");
 linkedList.printList();
-linkedList.addHead("I'm the head!");
+linkedList.addHead("Now I'm the head!!");
 linkedList.printList();
-linkedList.addHead("I'm the head! 2 2");
+linkedList.addHead("PSYCH! I'm the head now!!!");
 linkedList.printList();
-linkedList.addTail("I'm the Tail!");
-linkedList.printList();
-linkedList.getSize();
-linkedList.clear();
+linkedList.addTail("I'm the TAIL!!");
 linkedList.printList();
 
