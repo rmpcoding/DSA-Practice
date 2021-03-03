1b4a0be569285b6053fefb2eb1d69af68bd14bea
Robert Parsons <rmpcoding@gmail.com>
2021-03-03 12:33:57 -0800

    Adds Queue class skeletal structure with basic pseudocode

---
+++ Queue/Queue.js
@@ -0,0 +1,16 @@
+const LinkedList = require('./LinkedList');
+
+class Queue {
+    constructor() {
+        // Queue has the Queue itself as a property, which is actually a LinkedList
+        // Queue has a size property
+        // Queue has a maxSize property
+            // Defines whether Queue is bounded or not
+    }
+
+    // Methods by which we manipulate the Queue
+        // Enqueue
+        // Dequeue
+        // Peek
+
+}
