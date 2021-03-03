12a1d0fd27b7883e93c3919b2a57ffe5ee0b1381
Robert Parsons <rmpcoding@gmail.com>
2021-03-03 12:27:03 -0800

    Adds addTail() and removeHead() methods to LinkedList in Queue directory, for more practice

--- Queue/LinkedList.js
+++ Queue/LinkedList.js
@@ -16,10 +16,27 @@ class LinkedList {
     }
 
     removeHead() {
-
+        let removedHead = this.head;
+        let newHead = removedHead.getNextNode()
+        if (removedHead) {
+            this.head.setNextNode(newHead);
+            console.log(`${removedHead} was removed as the head, and ${newHead} is now the new head!`)
+        } else {
+            console.log('There is no head to remove!')
+        }
     }
 
-    addTail() {
+    addTail(data) {
+        let tail = this.head;
+        let newTail = new Node(data);
 
+        if (!tail) {
+            this.addHead(newTail)
+        } else {
+            while(tail.getNextNode()) {
+                tail = tail.getNextNode();
+            }
+            tail = tail.setNextNode(newTail)
+        }
     }
 }
