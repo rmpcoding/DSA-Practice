f3c0f1d58a6faec8b21f3a021d630815053707b8
Robert Parsons <rmpcoding@gmail.com>
2021-03-05 10:36:53 -0800

    Adds dequeue() method, and exports LinkedList class to Queue

--- Queue/LinkedList.js
+++ Queue/LinkedList.js
@@ -40,3 +40,5 @@ class LinkedList {
         }
     }
 }
+
+module.exports = LinkedList;
--- Queue/Queue.js
+++ Queue/Queue.js
@@ -3,7 +3,7 @@ const LinkedList = require('./LinkedList');
 class Queue {
     constructor() {
         // Queue has the Queue itself as a property, which is actually a LinkedList
-        this.queue = new LinkedList();
+        this.queue = new LinkedList;
         // Queue has a size property
         this.size = 0;
         // Queue has a maxSize property
@@ -19,6 +19,21 @@ class Queue {
             console.log(`${data} was added to the queue!`)
         }
         // Dequeue
+        dequeue() {
+            const dequeue = this.queue.removeHead();
+            this.size--;            
+            console.log(`'${dequeue}' was removed from the queue!`)
+        }
         // Peek
 
 }
+
+let queue = new Queue;
+
+queue.enqueue('data here')
+
+console.log(queue)
+
+queue.dequeue();
+
+console.log(queue)
