add0d5c8d889bd1af3f1e95b01f2596af6547398
Robert Parsons <rmpcoding@gmail.com>
2021-03-18 17:00:15 -0700

    Adds hash() method to HashMap class, and fixes module.export

--- Hash Maps/HashMap.js
+++ Hash Maps/HashMap.js
@@ -2,6 +2,22 @@ class HashMap {
     constructor(size = 0) {
         this.hashmap = new Array(size).fill(null);
     }
+
+    hash(key) {
+        let hashCode = 0;
+
+        for (let i = 0; i < key.length; i++) {
+            hashCode += hashCode + key.charCodeAt(i);
+        }
+
+        hashCode = hashCode % this.hashmap.length;
+
+        return hashCode;
+    }
 }
 
-modules.export = HashMap;
+const myHash = new HashMap(6)
+
+console.log(myHash.hash('hello'))
+
+module.export = HashMap;
