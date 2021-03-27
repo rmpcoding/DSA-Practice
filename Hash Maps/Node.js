7db6800814fdc8f638c51442c5fafc45f1806768
Robert Parsons <rmpcoding@gmail.com>
2021-03-27 00:04:00 -0700

    Adds setNextNode() method to Node class

--- Hash Maps/Node.js
+++ Hash Maps/Node.js
@@ -1,8 +1,15 @@
 class Node {
-    constructor(data = null) {
+    constructor(data) {
         this.data = data;
         this.next = null;
+    }
+
+    setNextNode(data) {
+        if (data instanceof Node || data === null) {
+            this.next = data;
+        } else throw new Error('Data must be a member of Node class!')
     }
 }
 
 module.exports = Node;
+
