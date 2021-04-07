c2f3a455c3b0daaa29ad71103d92eedbe513cf0d
Robert Parsons <rmpcoding@gmail.com>
2021-04-06 18:37:20 -0700

    Adds TreeNode skeletal structure, with constructor properties

---
+++ Trees/TreeNode.js
@@ -0,0 +1,12 @@
+class TreeNode {
+    constructor(data) {
+        this.data = data;
+        this.children= [];
+    }
+
+    // addChild method
+    // removeChild method
+    // depthFirstTraversal (recursive) method
+    // breadthFirstTraversal method
+
+}
