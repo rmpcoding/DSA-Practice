d56b217c7c7033c62682537022a219da8f994128
Robert Parsons <rmpcoding@gmail.com>
2021-04-14 14:05:49 -0700

    Adds existsInString() algorithm()

--- Strings/stringPlayGround.js
+++ Strings/stringPlayGround.js
@@ -1,3 +1,14 @@
 let sentence = 'The quick brown fox jumped over the barn yard, and it was impressive.'
 let red = 'red';
 let yellow = 'yellow';
+
+
+console.log(sentence.indexOf('q', 1));
+
+function existsInString(string, character) {
+    return string.indexOf(character) !== -1;
+}
+
+
+console.log(existsInString(red, 'r'));
+console.log(existsInString(red, 'y'));
