c3beb45e5bcbdea291c7bbe8a68ac57d0010b43c
Robert Parsons <rmpcoding@gmail.com>
2021-04-17 10:53:47 -0700

    Adds removeChild() method to TreeNode class

--- Numbers/numberPlayGround.js
+++ Numbers/numberPlayGround.js
@@ -1,4 +1,22 @@
-const array = [1, 2, 3, 4, -95];
+let array = [1, 2, 3, 4, -95];
 
-console.log(Math.min(...array));
-console.log(Number.MIN_VALUE);
+// console.log(Math.min(...array));
+// console.log(Number.MIN_VALUE);
+
+// hoistExample('Hello, I am hoisted yo!');
+
+// function hoistExample(data) {
+//     console.log(data);
+// }
+
+// const hoistExample = (data) => {
+//     console.log(data);
+// };
+
+
+array = array.filter((num) => {
+    return num > 0;
+});
+
+console.log(array);
+// console.log(filteredArray);
--- Trees/TreeNode.js
+++ Trees/TreeNode.js
@@ -15,6 +15,39 @@ class TreeNode {
             this.children.push(new TreeNode(node));
         }
     }
+
+    removeChild(node) {
+        // create a children variable referring to children array
+        // keep original length of our array by
+            // creating a const of the children's array length
+        // check if node is an instance of our TreeNode class
+            // filter over this children array to
+                //  return a new array without the node which matches our passed in node
+        // or else
+            // filter over this children array to 
+                // return a new array without the node which matches our passed in node's data property.
+        // if our filter method works, it will reduce our array by one
+        // as such, if our length constant is the same as our instance of children array's length
+            // recurse for each child in the array, calling on the removeChild() method.
+
+        let children = this.children;
+        const length = this.children.length;
+
+        children = children.filter(child => {
+            if (node instanceof TreeNode) {
+                return child !== node;
+            } else return child.data !== node;
+        })
+
+        if (length === this.children.length) {
+            this.children.forEach(child => {
+                child.removeChild(node);
+            })
+        }
+        
+
+
+    }
     
     print(level = 0) {
         let result = '';
