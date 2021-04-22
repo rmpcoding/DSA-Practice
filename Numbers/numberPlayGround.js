4af1cc4ef753f3c90e3170b43e518cb193d8892c
Robert Parsons <rmpcoding@gmail.com>
2021-04-21 20:01:04 -0700

    Adds performance.js file and uses process.hrtime() to benchmark for illustration

--- Numbers/numberPlayGround.js
+++ Numbers/numberPlayGround.js
@@ -13,10 +13,39 @@ let array = [1, 2, 3, 4, -95];
 //     console.log(data);
 // };
 
-
 array = array.filter((num) => {
     return num > 0;
 });
 
-console.log(array);
+// console.log(array);
 // console.log(filteredArray);
+
+function addNumbersForLoop(num) {
+    let total = 0;
+    for (let i = 1; i <= num; i++) {
+        total += i;
+    }
+    return total;
+}
+
+const NS_PER_SEC = 1e9;
+const time = process.hrtime();
+
+console.log(time);
+
+const difference = process.hrtime(time);
+
+console.log(addNumbersForLoop(60000000));
+
+console.log(difference);
+
+const performanceInNanoSeconds = difference[0] * NS_PER_SEC + difference[1];
+
+const seconds = performanceInNanoSeconds / 1000000000;
+
+console.log(seconds);
+
+console.log(`
+Performance: ${performanceInNanoSeconds} nanoseconds
+Performance: ${seconds} seconds
+`);
---
+++ performance.js
@@ -0,0 +1,25 @@
+const NS_PER_SEC = 1e9;
+const time = process.hrtime();
+
+function addNumbersForLoop(num) {
+    let total = 0;
+    for (let i = 1; i <= num; i++) {
+        total += i;
+    }
+    return total;
+}
+
+console.log(addNumbersForLoop(60000000));
+
+const difference = process.hrtime(time);
+
+const performanceInNanoSeconds = difference[0] * NS_PER_SEC + difference[1];
+
+const seconds = performanceInNanoSeconds / 1000000000;
+
+console.log(seconds);
+
+console.log(`
+Performance: ${performanceInNanoSeconds} nanoseconds
+Performance: ${seconds} seconds
+`);
