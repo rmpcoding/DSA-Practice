d7200eed072718def1b6c5b702046e74b54203ec
Robert Parsons <rmpcoding@gmail.com>
2021-04-30 08:47:25 -0700

    Adds find() method to BST_01

--- BinarySearchTrees/BST_01.js
+++ BinarySearchTrees/BST_01.js
@@ -11,38 +11,25 @@ class BinarySearchTree_01 {
         this.root = null;
     }
 
-
     find(node) {
-        // return boolean  
-        // if there is a root
-            // if not, search is over;
-        // if there is a root, check if node is equal to root's value
-            // if so, search is over;
-        // create variable current set equal to this root
-        // create a while loop
-            // while true
-        // if there is a root, check if node is greater than root
-            // if it is greater
-                // check to see if there is a node to the right;
-                // if so, current will be current's right node
-                // if not, add that node as current's right node
-            // if it is less
-                // check to see if there is a node to the left;
-                // if so, move to that node and commence new iteration
-                // if not, that's our node;
+        if (!this.root) return false;
+        if (!(node instanceof BST_Node_01)) node = new BST_Node_01(node);
+        if (this.root.value === node.value) return true;
+
+        let current = this.root;
+
+        while (true) {
+            if (node.value === current.value) return true;
+
+            if (node.value < current.value) {
+                if (!current.left) return false;
+                current = current.left;
+            } else {
+                if (!current.right) return false;
+                current = current.right;
+            }
+        }
     }
-
-
-
-
-
-
-
-
-
-
-
-
 
     insert(node) {
         let instantiatedNode;
@@ -55,18 +42,18 @@ class BinarySearchTree_01 {
 
         if (!this.root) {
             this.root = instantiatedNode;
-            console.log(this)
+            // console.log(this)
             return;
         }
 
         let current = this.root;
 
         while (true) {
-            if (instantiatedNode.value === current.value) return undefined
+            if (instantiatedNode.value === current.value) return undefined;
             if (instantiatedNode.value < current.value) {
                 if (!current.left) {
                     current.left = instantiatedNode;
-                    console.log(this)
+                    // console.log(this)
                     return this;
                 }
                 current = current.left;
@@ -84,13 +71,19 @@ class BinarySearchTree_01 {
 let tree = new BinarySearchTree_01();
 
 tree.insert(100);
-tree.insert(50);
-tree.insert(25);
-tree.insert(27);
-tree.insert(15);
+tree.insert(10);
+tree.insert(75);
 tree.insert(125);
-tree.insert(225);
-tree.insert(220);
-tree.insert(525);
+tree.insert(110);
+// tree.insert(125);
+// tree.insert(225);
+// tree.insert(220);
+// tree.insert(525);
 
-console.log(JSON.stringify(tree));
+console.log(tree.find(100))
+console.log(tree.find(10))
+console.log(tree.find(75))
+console.log(tree.find(125))
+console.log(tree.find(110))
+
+// console.log(JSON.stringify(tree));
