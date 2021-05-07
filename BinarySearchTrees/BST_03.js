9d5d6fa4307c4904430e1fabf3569732896a7e4f
Robert Parsons <rmpcoding@gmail.com>
2021-05-07 15:13:00 -0700

    Adds find() method, but it only returns true or false instead of the node itself

--- BinarySearchTrees/BST_03.js
+++ BinarySearchTrees/BST_03.js
@@ -34,6 +34,40 @@ BinarySearchTree02.prototype.insert = function (node) {
     }
 };
 
+BinarySearchTree02.prototype.find = function(node) {
+    // if this root does not exist, return false
+    // if this root's value IS our node to find, return true
+    // create a variable current set equal to this tree's root
+    // create while loop to run under condition of true
+        // if current node's value is strictly equal to node
+            // return true;
+        // if node is less than current node's value
+            // if current's left node does NOT exist
+                // return false
+            // set current equal to current's left node
+        // else
+            // if current's right node does not exist
+                // return false
+            // set current equal to current's right node
+
+    if (this.root === null) return false;
+    if (this.root.value === node) return true;
+    
+    let current = this.root;
+
+    while(true) {
+        if (current.value === node) return true;
+        if (node < current.value) {
+            if (!current.left) return false;
+            current = current.left;
+        } else {
+            if (!current.right) return false;
+            current = current.right;
+        }
+    }
+        
+}
+
 BinarySearchTree02.prototype.breadthFirstSearch = function () {
     const queue = [this.root];
     const results = [];
@@ -50,5 +84,13 @@ BinarySearchTree02.prototype.breadthFirstSearch = function () {
 
 let tree = new BinarySearchTree02();
 
-console.log(tree.insert(100))
+tree.insert(100)
+tree.insert(50)
+tree.insert(75)
+tree.insert(115)
+tree.insert(110)
+tree.insert(125)
+tree.insert(25)
+tree.insert(10)
 
+console.log(tree.find(9))
