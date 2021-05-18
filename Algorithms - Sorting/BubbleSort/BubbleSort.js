9fd26892968c4cf004f2237ed7241111081d4f04
Robert Parsons <rmpcoding@gmail.com>
2021-05-17 21:16:44 -0700

    Adds bubbleSort() algorithm

--- Algorithms - Sorting/BubbleSort/BubbleSort.js
+++ Algorithms - Sorting/BubbleSort/BubbleSort.js
@@ -1,8 +1,19 @@
 const bubbleSort = input => {
+    let swapping = true;
 
+    while (swapping) {
+        swapping = false;
+        for (let i = 0; i < input.length - 1; i++) {
+            if (input[i] > input[i + 1]) {
+                swap(input, i, i + 1);
+                swapping = true;
+            }
+        }
+    }
+    return input;
 }
 
-const swapping = (array, indexOne, indexTwo) => {
+const swap = (array, indexOne, indexTwo) => {
     const temp = array[indexTwo];
     array[indexTwo] = array[indexOne];
     array[indexOne] = temp;
