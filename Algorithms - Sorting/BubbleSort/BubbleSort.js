a88144188d7978fc03725dd52ce1bbfba82b99e4
Robert Parsons <rmpcoding@gmail.com>
2021-05-17 21:06:33 -0700

    Adds swapping() function to bubbleSort() algorithm

--- Algorithms - Sorting/BubbleSort/BubbleSort.js
+++ Algorithms - Sorting/BubbleSort/BubbleSort.js
@@ -3,5 +3,7 @@ const bubbleSort = input => {
 }
 
 const swapping = (array, indexOne, indexTwo) => {
-    
+    const temp = array[indexTwo];
+    array[indexTwo] = array[indexOne];
+    array[indexOne] = temp;
 }
