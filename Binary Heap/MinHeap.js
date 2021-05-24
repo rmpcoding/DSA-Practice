99817b191da09bb29fe3978cbd94c836ebffe5ce
Robert Parsons <rmpcoding@gmail.com>
2021-05-23 22:10:55 -0700

    Adds heapify() method to MinHeap class, as well as helper functions

--- Binary Heap/MinHeap.js
+++ Binary Heap/MinHeap.js
@@ -13,15 +13,52 @@ class MinHeap {
         this.size++;
         this.bubbleUp();
     }
-    
+
     popMin() {
         if (!this.size) return null;
 
-        this.swap(1, this.size);
+        this.swap(0, this.size);
         const min = this.heap.pop();
         this.size--;
         this.heapify(); // this doesn't exist yet
         return min;
+    }
+
+    exists(index) {
+        return index <= this.size;
+    }
+
+    canSwap(current, leftChild, rightChild) {
+        return (
+            (this.exists(leftChild) &&
+                this.heap[current] > this.heap[leftChild]) ||
+            (this.exists(rightChild) &&
+                this.heap[current] > this.heap[rightChild])
+        );
+    }
+
+    heapify() {
+        let current = 0;
+        let leftChild = getLeft(current);
+        let rightChild = getRight(current);
+
+        while (this.canSwap(current, leftChild, rightChild)) {
+            if (this.exists(leftChild) && this.exists(rightChild)) {
+                if (leftChild < rightChild) {
+                    this.swap(current, leftChild);
+                    current = getLeft(current);
+                } else {
+                    this.swap(current, rightChild);
+                    current = getRight(current);
+                }
+            } else {
+                this.swap(current, getLeft(current));
+                current = getLeft(current);
+            }
+
+            leftChild = getLeft(current);
+            rightChild = getRight(current);
+        }
     }
 
     swap(a, b) {
