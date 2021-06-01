e8dbb8b255caf206df0d8e518f9a856f6ef698c6
Robert Parsons <rmpcoding@gmail.com>
2021-05-31 20:18:54 -0700

    Exports Vertex class

--- Graphs/Vertex.js
+++ Graphs/Vertex.js
@@ -6,15 +6,15 @@ class Vertex {
         this.edges = [];
     }
 
-    // addEdge();
     addEdge(vertex, weight) {
         if (!(node instanceof Vertex))
             throw new Error('Expected an instantiated Vertex as argument');
         this.edges.push(new Edge(this, vertex, weight));
     }
-    // removeEdge();
+
     removeEdge(vertex) {
         this.edges = this.edges.filter((edge) => edge.end !== vertex);
     }
-    // print();
 }
+
+module.exports = Vertex;
