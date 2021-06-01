fbf6dc0b5ed66136f7b19d2afd2116fbe1da06e4
Robert Parsons <rmpcoding@gmail.com>
2021-05-31 19:53:22 -0700

    Adds Edge class and exports it

---
+++ Graphs/Graph.js
@@ -0,0 +1,24 @@
+/* -------------------------------------------------------------------------- */
+/*                                 GRAPH CLASS                                */
+/* -------------------------------------------------------------------------- */
+
+
+/* --------------------------------------------------------------------------
+
+A Vertex can store any data.
+
+A Vertex maintains a list of connections to other vertices, represented by a list of Edge instances.
+
+A Vertex can add and remove edges going to another Vertex.
+
+A Graph stores all of its vertices, represented by a list of Vertex instances.
+
+A Graph knows if it is directed or undirected.
+
+A Graph knows if it is weighted or unweighted.
+
+A Graph can add and remove its own vertices.
+
+A Graph can add and remove edges between stored vertices.
+
+-------------------------------------------------------------------------- */
