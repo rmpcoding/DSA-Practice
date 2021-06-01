642b9f8be86ab4f41c88e5c3e8252bd01df6e132
Robert Parsons <rmpcoding@gmail.com>
2021-06-01 15:42:09 -0700

    Adds Edge class in current (today's date) directory

---
+++ Graphs/Graphs_06_01_2021/01/2021/Edge.js
@@ -0,0 +1,9 @@
+class Edge_01 {
+    constructor(start, end, weight = null) {
+        this.start = start;
+        this.end = end;
+        this.weight = weight;
+    }
+}
+
+module.exports = Edge_01
