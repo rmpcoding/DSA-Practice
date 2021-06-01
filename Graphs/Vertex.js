1bc8bdb91409540b1551bf759a28167002d9b04c
Robert Parsons <rmpcoding@gmail.com>
2021-05-31 20:14:55 -0700

    Adds addEdge() method to Vertex class

--- Graphs/Vertex.js
+++ Graphs/Vertex.js
@@ -1,6 +1,17 @@
+const Edge = require('./Edge');
+
 class Vertex {
     constructor(data) {
         this.data = data;
         this.edges = [];
     }
+
+    // addEdge();
+    addEdge(vertex, weight) {
+        if (!(node instanceof Vertex))
+            throw new Error('Expected an instantiated Vertex as argument');
+        this.edges.push(new Edge(this, vertex, weight));
+    }
+    // removeEdge();
+    // print();
 }
